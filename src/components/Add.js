37b556fde5d900af0ac537fd42f71176a73e00d2
Karel Braborec <karel.braborec@office365.spspzlin.cz>
2024-02-03 15:10:36 +0100

    pridani novinek na domovskou stranku a pridani novinek

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,19 +1,66 @@
+import { useState } from "react"
 import "./Add.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const Add = () => {
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+  
+    const [feedback, setFeedback] = useState()
+
+    const onSubmitHandler = event => {
+        event.preventDefault();
+    
+        const newArticle = {
+          title: title,
+          text: text,
+        }
+    
+    
+        fetch('http://localhost:8000/news', {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newArticle) 
+        }).then(r=> r.json())
+          .then(json => setFeedback(json))
+          .finally(()=> {
+            
+            toast.success("Článek byl přidán do košíku", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+            setTitle("");
+            setText("");
+          });
+    
+    
+      }
+
     return ( <div className="add">
         <h2 className="add-heading">Přidat článek na domovskou stránku</h2>
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <label>Typ</label>
             <select name="sd" id="type-input">
                 <option value="clanek">Článek</option>
             </select>
             <label >Název článku</label>
-            <input id="title-input" type="text" />
+            <input id="title-input" type="text" onChange={(e) => setTitle(e.target.value)} />
             <label>Obsah článku</label>
-            <textarea name="" id="textarea-input" cols="50" rows="5"></textarea>
+            <textarea name="" id="textarea-input" cols="50" rows="5" onChange={(e) => setText(e.target.value)}></textarea>
             <button type="submit" id="submit-button">Potvrdit</button>
         </form>
+        <ToastContainer/>
     </div> );
 }
  
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import "./Home.css"
+import useFetch from "./useFetch";
+
+
 
 const Home = () => {
+    const {data: articles, isPending, error} = useFetch("http://localhost:8000/news")
+
     return ( <div className="home">
         <div className="bg-container"></div>
         <h2 className="news-heading">Novinky</h2>
+        {articles.map(article => <div key={article.id} className="news-item">
+            <h3 className="news-item-title">{article.title}</h3>
+            <p className="news-item-text">{article.text}</p>
+        </div>)}
+        
     </div> );
 }
  
